d673ea138db0253a3aafd943ea52b1d05f0495e0
ingveliz02ia <ingveliz02ia@gmail.com>
2024-11-07 19:30:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from server import app
 #If you want to use the server for "reload" you should change "app" and put "server:app" and then add
 #reload=True.
 if __name__ == '__main__':
-	uvicorn.run(app, host="https://voice-production-2147.up.railway.app/", port=80)
+	uvicorn.run(app, host="0.0.0.0", port=80)
